625617080c512d1f810c7edbbdca3c910fd92d3a
nplayer89 <n.player89@gmail.com>
2023-08-07 17:29:30 -0600

    implement loop

--- tokio/tokio_async/src/main.rs
+++ tokio/tokio_async/src/main.rs
@@ -1,14 +1,13 @@
 use tokio::net::TcpListener;
-use std::io::Result;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
-    
-    match listener.accept().await {
-        Ok((_socket, addr)) => println!("new client: {:?}", addr),
-        Err(e) => println!("couldn't get client: {:?}", e),
-    }
-    
-    Ok(())
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+
+   loop { 
+        match listener.accept().await {
+            Ok((_socket, addr)) => println!("new client: {:?}", addr),
+            Err(e) => println!("couldn't get client: {:?}", e),
+        }
+   }
 }
